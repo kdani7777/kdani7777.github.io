2cf9004103c57c6b2ab92b97f892d2e5e22ff097
Kush Dani <kushdani@Kushs-MacBook-Air.local>
2024-01-22 20:44:30 -0800

    Removed outline from clicking hamburger menu

--- app/ui/nav-mobile.jsx
+++ app/ui/nav-mobile.jsx
@@ -9,7 +9,7 @@ export default function NavMobile() {
 
     return (
         <div className="fixed top-3 left-3">
-            <Hamburger toggled={isOpen} toggle={setOpen} size={20} />
+            <Hamburger rounded toggled={isOpen} toggle={setOpen} size={20} label="Show menu" hideOutline={true} />
         </div>
     );
 }
