f7c90a8b699a1d11a4a7c22f2a4c532e61397b3a
Kush Dani <kushdani@Kushs-MacBook-Air.local>
2024-02-01 13:57:16 -0800

    changed subtitle message on reading page

--- app/reading/page.js
+++ app/reading/page.js
@@ -9,8 +9,8 @@ export default function BookGrid() {
       <NavMobile />
       <div className="container mx-auto p-4 mt-20">
         <h1 className="text-3xl text-center my-4 md:my-8 font-bold">books i have read</h1>
-        <p className="text-center mb-4 font-semibold">(as of January 31, 2024)</p>
-        <div className="grid grid-cols-3 gap-4 p-4 md:p-8 border-solid border-indigo-400 border-2">
+        <p className="text-center mb-4 font-semibold">(in recent memory)</p>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 md:p-8">
           {books.map((book) => (
             <Book key={book.id} {...book} />
           ))}
