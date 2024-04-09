dfbee30047b12e8a78bb13432accf3d52f128306
Kush Dani <kushdani@Kushs-MacBook-Air.local>
2024-04-09 16:20:11 -0400

    finished billion dollar whale

--- app/reading/book-data.js
+++ app/reading/book-data.js
@@ -36,4 +36,5 @@ export const books = [
     { id: 35, src: 'shoe-dog-cover', alt: 'Shoe Dog cover', title: 'Shoe Dog', author: 'Phil Knight', link: 'https://www.goodreads.com/en/book/show/27220736' },
     { id: 36, src: 'brave-new-world-cover', alt: 'Brave New World cover', title: 'Brave New World', author: 'Aldous Huxley', link: 'https://www.goodreads.com/book/show/5129.Brave_New_World' },
     { id: 37, src: 'once-a-runner-cover', alt: 'Once A Runner cover', title: 'Once A Runner', author: 'John L. Parker Jr', link: 'https://www.goodreads.com/book/show/98250.Once_a_Runner' },
+    { id: 38, src: 'billion-dollar-whale-cover', alt: 'Billion Dollar Whale cover', title: 'Billion Dollar Whale', author: 'Tom Wright and Bradley Hope', link: 'https://www.goodreads.com/en/book/show/38743564' },
 ]
